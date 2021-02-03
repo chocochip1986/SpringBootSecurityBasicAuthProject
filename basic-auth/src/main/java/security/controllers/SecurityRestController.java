21bd2dad89a7c40a922e76d1eccd3b35b290bc98
Ou Guozheng <abc@abc.com>
2021-02-03 10:55:44 +0800

    stuff

--- basic-auth/src/main/java/security/controllers/SecurityRestController.java
+++ basic-auth/src/main/java/security/controllers/SecurityRestController.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import security.entities.Person;
 import security.jpa.PersonJpaRepo;
@@ -15,12 +18,17 @@ public class SecurityRestController {
     @Autowired
     private PersonJpaRepo personJpaRepo;
 
-    @GetMapping(value = "/person")
-    public ResponseEntity<String> createPerson() {
+    @GetMapping(value = "/person/{name}")
+    public ResponseEntity<String> person(@PathVariable("name") String name) {
+        return new ResponseEntity<>(personJpaRepo.findByName(name).orElse(null).toString(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/person")
+    public ResponseEntity<String> person(){
         return new ResponseEntity<>(personJpaRepo.save(Person.builder().name("Name "+System.currentTimeMillis()).build()).getName(), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/list/all")
+    @GetMapping(value = "/person/list-all")
     public ResponseEntity<String> getPersons(Authentication authentication) {
         System.out.println("User: " + authentication.getName());
         return new ResponseEntity<String>(personJpaRepo.findAll().stream().map(Person::getName)
