c06f112f216096a5271b0a367b97a626db2df1a2
Ou Guozheng <abc@abc.com>
2021-02-03 10:17:33 +0800

    Transformed project into multi module project

--- basic-auth/src/main/java/security/controllers/SecurityRestController.java
+++ basic-auth/src/main/java/security/controllers/SecurityRestController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import security.entities.Person;
 import security.jpa.PersonJpaRepo;
 
-import java.util.List;
-
 @RestController
 public class SecurityRestController {
 
